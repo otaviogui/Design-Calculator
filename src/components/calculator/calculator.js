317fab7ea2ac4eb75d42abdbc8d81ea951f6501d
otavio <otavio.ferreira@lotuz.de>
2020-06-07 13:57:23 -0300

    test prettier

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import * as S from "./styled";
 
@@ -9,40 +9,51 @@ import Plus from "../../icons/plus.svg";
 import Equal from "../../icons/equal.svg";
 
 function Calculator() {
+  const [number, setNumber] = useState([]);
+
+  function SetNewState(num) {
+    let n = num;
+    setNumber(n);
+    let addNumber = [number, ...n]
+    console.log(addNumber)
+    setNumber(addNumber)
+  }
+
   return (
     <>
       <S.Wrap>
         <S.WindowResult>
-          <S.BeforeRes>
-            20 + 8 + 3 - 2
-          </S.BeforeRes>
-          <S.InitialRes>
-            20
-          </S.InitialRes>
-        
+          <S.BeforeRes>20 + 8 + 3 - 2</S.BeforeRes>
+          <S.InitialRes>{number}</S.InitialRes>
         </S.WindowResult>
         <S.WrapOptions>
-          <S.Option>c</S.Option>
-          <S.Option>
+          <S.Option onClick={() => setNumber("")}>c</S.Option>
+          <S.Option onClick={() => SetNewState("%")}>
             <Icon icon={Porcent} />
           </S.Option>
-          <S.Option>/</S.Option>
-          <S.Option>X</S.Option>
-          <S.Option>7</S.Option>
-          <S.Option>8</S.Option>
-          <S.Option>9</S.Option>
-          <S.Option><Icon icon={Minus} /></S.Option>
-          <S.Option>4</S.Option>
-          <S.Option>5</S.Option>
-          <S.Option>6</S.Option>
-          <S.Option><Icon icon={Plus} /></S.Option>
-          <S.Option>1</S.Option>
-          <S.Option>2</S.Option>
-          <S.Option>3</S.Option>
-          <S.Option>0</S.Option>
-          <S.Option>.</S.Option>
-          <S.Option>ap</S.Option>
-          <S.OptionEqual><Icon icon={Equal} /></S.OptionEqual>
+          <S.Option onClick={() => SetNewState("/")}>/</S.Option>
+          <S.Option onClick={() => SetNewState("x")}>X</S.Option>
+          <S.Option onClick={() => SetNewState("7")}>7</S.Option>
+          <S.Option onClick={() => SetNewState("8")}>8</S.Option>
+          <S.Option onClick={() => SetNewState("9")}>9</S.Option>
+          <S.Option onClick={() => SetNewState("-")}>
+            <Icon icon={Minus} />
+          </S.Option>
+          <S.Option onClick={() => SetNewState("4")}>4</S.Option>
+          <S.Option onClick={() => SetNewState("5")}>5</S.Option>
+          <S.Option onClick={() => SetNewState("6")}>6</S.Option>
+          <S.Option onClick={() => SetNewState("+")}>
+            <Icon icon={Plus} />
+          </S.Option>
+          <S.Option onClick={() => SetNewState("1")}>1</S.Option>
+          <S.Option onClick={() => SetNewState("2")}>2</S.Option>
+          <S.Option onClick={() => SetNewState("3")}>3</S.Option>
+          <S.Option onClick={() => SetNewState("0")}>0</S.Option>
+          <S.Option onClick={() => SetNewState(".")}>.</S.Option>
+          <S.Option onClick={() => SetNewState("")}>ap</S.Option>
+          <S.OptionEqual>
+            <Icon icon={Equal} />
+          </S.OptionEqual>
         </S.WrapOptions>
       </S.Wrap>
     </>
--- src/components/calculator/styled.js
+++ src/components/calculator/styled.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const Wrap = styled.div`
-  background:  #181818;
+  background: #181818;
   width: 300px;
   padding: 5px;
   margin: 0 auto;
@@ -13,44 +13,45 @@ export const WindowResult = styled.div`
   padding: 10px;
   color: #eaebed;
   text-align: right;
-  height: 100px;
+  min-height: 100px;
+  max-width: 290px;
+  overflow: hidden;
 `;
 
 export const InitialRes = styled.div`
-font-size: 3rem;
-
+  font-size: 3rem;
+  padding: 10px;
 `;
 
 export const BeforeRes = styled.div`
-height: 50px;
-font-size: 1rem;
+  height: 50px;
+  font-size: 1rem;
 `;
 
-
 export const WrapOptions = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap:wrap;
-    padding: 2px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 2px;
 `;
 
 export const Option = styled.div`
-    border: 0;
-    font-size: 1rem;
-    text-align: center;
-    padding:30px;
-    border-radius: 5px;
-   background: #333333;
-    margin-top: 5px;
-    color: #eaebed;
-    width:10px;
-    height: 20px;
-    &:hover{
-        background:#161616;
-    }
-    cursor: pointer;
+  border: 0;
+  font-size: 1rem;
+  text-align: center;
+  padding: 30px;
+  border-radius: 5px;
+  background: #333333;
+  margin-top: 5px;
+  color: #eaebed;
+  width: 10px;
+  height: 20px;
+  &:hover {
+    background: #161616;
+  }
+  cursor: pointer;
 `;
 
 export const OptionEqual = styled(Option)`
-    width: 85px;
+  width: 85px;
 `;
